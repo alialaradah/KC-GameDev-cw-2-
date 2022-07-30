106a7f4d6622fad2f8768a36567cf22ca8343a2c
alialaradah <100695337+alialaradah@users.noreply.github.com>
2022-07-30 16:02:50 +0300

    Hero & Villain info

---
+++ CW2/CW2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CW2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+        // Naruto's info
+        string HeroName = " Uzumaki Naruto ";
+        int HeroHight = 180;
+        int HeroAge = 32;
+        bool HeroSuperPower = true;
+
+
+        // Madara's info
+        string VillainName = " Uchiha Madara ";
+        int VillainHight = 179;
+        int VillainAge = 90;
+        bool VillainSuperPower = true;
+
+
+
+        int ageDifference = VillainAge - HeroAge;
+
+
+
+
+
+            Console.WriteLine(" Our Hero Name is " + HeroName + "and he is " + HeroAge + " years old, ");
+            Console.WriteLine(" his hight is " + HeroHight + "cm and he got a " + HeroSuperPower + " SuperPower!");
+
+
+
+
+
+
+
+
+            Console.WriteLine(" One of the strongest enemies that Naruto fought is "+ VillainName + " and his real age is " + VillainAge +" years old, ");
+            Console.WriteLine(" his hight is " + VillainHight + "cm and he got a " + VillainSuperPower + " SuperPowers!");
+
+
+            Console.WriteLine(" the age difference between them is " + ageDifference + " years old");
+        }
+    }
+}
